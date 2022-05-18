df039a6d69a1a2b6bba4f19e8dab75ccd2734e76
PY <51064667+pyxn@users.noreply.github.com>
2022-05-18 10:54:31 -0400

    Update WestJetWeek.js

--- touche/WestJet/WestJetWeek.js
+++ touche/WestJet/WestJetWeek.js
@@ -1,1 +1,19 @@
-test
+var weekOffset = {
+	'Wed': -5,
+	'Thu': -6,
+	'Fri': 0,
+	'Sat': -1,
+	'Sun': -2,
+	'Mon': -3,
+	'Tue': -4
+};
+
+var dayOfWeek = FORMATDATE([Day], 'EEE');
+
+var startOfWeek = DATEADD('d', weekOffset[dayOfWeek], [Day])
+var startOfWeekFormatted = FORMATDATE(startOfWeek, 'MMM dd');
+
+var endOfWeek = DATEADD('d', 6, startOfWeek)
+var endOfWeekFormatted = FORMATDATE(endOfWeek, 'MMM dd');
+	
+return startOfWeekFormatted + ' - ' + endOfWeekFormatted
