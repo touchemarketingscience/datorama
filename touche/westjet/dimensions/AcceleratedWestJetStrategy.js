a12e5b532416862e8ee7bb3e398bdc08401d91f7
PY <51064667+pyxn@users.noreply.github.com>
2022-07-19 08:34:49 -0400

    Delete AcceleratedWestJetStrategy.js

--- touche/westjet/dimensions/AcceleratedWestJetStrategy.js
+++
@@ -1,9 +0,0 @@
-if ([WestJet_Site] == 'SA360')
-	return 'Search'
-
-if ([WestJet_Strategy_Cleaned] == 'AW')  return 'Awareness'
-if ([WestJet_Strategy_Cleaned] == 'CN')  return 'Consideration'
-if ([WestJet_Strategy_Cleaned] == 'CO')  return 'Conversion'
-
-
-return '"' + [WestJet_Strategy_Cleaned] + '"' + ' | S: ' + [WestJet_Source] + ' | P: ' + [Media_Buy_Name]
